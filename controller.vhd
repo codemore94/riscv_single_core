b6ceb6c448759feea830e7600b54b07bcde31ae8
Mikko Saari <mikko.l.saari@tuni.fi>
2024-07-28 06:18:59 +0300

    controller_init_definition

--- controller.vhd
+++ controller.vhd
@@ -2,4 +2,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity controller is
-  port(op:
+  port(op:in std_logic_vector(6 downto 0);
+       funct3:in std_logic_vector(2 downto 0);
+       funct7b5,Zero: in std_logic;
+       ResultSrc: out std_logic_vector(1 downto 0);
+       MemWrite: out std_logic;
+       
